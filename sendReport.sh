7c9a3294bd3db9f19045cdd39428adea7c0f9cf2
Teka101 <teka101+github@gmail.com>
2016-03-30 14:06:59 +0200

    Test GIST integration MD

--- sendReport.sh
+++ sendReport.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+REPORT_FILE=$1
+
 cat > report.json <<EOF
 {
   "description": "Report Travis-CI $TRAVIS_JOB_NUMBER",
   "files": {
-    "report.html" : { "content": "$(cat report.html | sed -r -e 's/"/\\"/g' | tr '\n' ' ')" }
+    "$REPORT_FILE" : { "content": "$(cat $REPORT_FILE | sed -r -e 's/"/\\"/g' -e 's/$/\\n/' | tr -d '\n')" }
   }
 }
 EOF
 
-curl -X PATCH --data-binary @report.json -H "Authorization: token $GIST_TOKEN" 'https://api.github.com/gists/94c7b6a4408e1f601899'
+curl -X PATCH --data-binary @$REPORT_FILE -H "Authorization: token $GIST_TOKEN" 'https://api.github.com/gists/94c7b6a4408e1f601899'
