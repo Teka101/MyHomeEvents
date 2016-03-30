7d2f4534097f99bfd133f9556993108d84c0aa60
Teka101 <teka101+github@gmail.com>
2016-03-30 12:58:38 +0200

    Test GIST integration....

--- sendReport.sh
+++ sendReport.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-curl -H "Authorization: token $GIST_TOKEN" 'https://api.github.com/gists'
+DATA="
+{
+  \"description\": \"Report Travis-CI $TRAVIS_JOB_NUMBER\"
+  \"files\": { \"report.html\" : { \"content\": \"$(cat report.html | sed -r -e 's/"/\\"/g')\" } }
+}"
+
+
+curl -X PATCH --data-binary "$DATA" -H "Authorization: token $GIST_TOKEN" 'https://api.github.com/gists/94c7b6a4408e1f601899'
