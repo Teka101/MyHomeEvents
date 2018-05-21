ec31af7da515a4c1928889c7a41cd966677575cd
Teka101 <teka101+github@gmail.com>
2018-05-21 12:48:53 +0200

    Add missing include

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <sstream>
 #include <string>
 
+#include <boost/exception/all.hpp>
 #include <boost/foreach.hpp>
 #include <boost/program_options.hpp>
 #include <log4cplus/configurator.h>
